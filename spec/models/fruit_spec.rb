0bb5ad9f379a42bab4cdb735d61956d376a3a7ab
Ayumu <ths40374@gmail.com>
2018-08-09 12:31:40 +0900

    Update fruit_spec.rb

--- spec/models/fruit_spec.rb
+++ spec/models/fruit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Fruit, type: :model do
   it "フルーツが桃であるか" do
     fruit = Fruit.new(
-      fruits_name: "ももです",
+      fruits_name: "もも",
     )
     expect(fruit.peach_name).to eq fruit.fruits_name
   end
